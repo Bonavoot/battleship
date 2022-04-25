1b4123721bf91d9b5817381f9a0bc725609b3345
Joey Bonventre <Jbonventre95@gmail.com>
2022-04-25 18:36:45 -0400

    Letter and Num Coordinates added to boards

--- src/renderBoard.js
+++ src/renderBoard.js
@@ -6,18 +6,63 @@ let renderBoard = function (gameBoardArray) {
   const playerTwoBoard = document.createElement("div");
   playerTwoBoard.classList.add("player-two-board");
   document.body.appendChild(playerTwoBoard);
+  let index = 0;
+  let letter = "A";
+  let counter = 0;
 
-  // Places array elements as cells in the DOM
+  // Places array elements as cells in the DOM for the HUMAN board
   gameBoardArray.forEach((elem) => {
     let cell = document.createElement("div");
     cell.classList.add("player-one-cell");
+    index++;
+
+    // Adds letters on Y axis of grid
+    if (index < 11) {
+      let coordLetter = document.createElement("div");
+      coordLetter.textContent = letter;
+      coordLetter.classList.add("coord-letter");
+      cell.appendChild(coordLetter);
+      letter = String.fromCharCode(letter.charCodeAt(0) + 1);
+    }
+
+    //Adds numbers on X axis of grid
+    if (index % 10 == 0) {
+      ++counter;
+      let coordNum = document.createElement("div");
+      coordNum.classList.add("coord-num");
+      coordNum.textContent = counter;
+      cell.appendChild(coordNum);
+    }
     cell.setAttribute("id", `${elem}`);
     playerOneBoard.appendChild(cell);
   });
 
+  // Places array elements as cells in the DOM for the COMPUTER board
+  let indexCPU = 0;
+  let letterCPU = "A";
+  let counterCPU = 0;
+
   gameBoardArray.forEach((elem) => {
     let cell = document.createElement("div");
     cell.classList.add("computer-cell");
+    indexCPU++;
+    // Adds letters on Y axis of grid
+    if (indexCPU < 11) {
+      let coordLetter = document.createElement("div");
+      coordLetter.textContent = letterCPU;
+      coordLetter.classList.add("coord-letter-cpu");
+      cell.appendChild(coordLetter);
+      letterCPU = String.fromCharCode(letterCPU.charCodeAt(0) + 1);
+    }
+    //Adds numbers on X axis of grid
+    if (indexCPU % 10 == 0) {
+      ++counterCPU;
+      let coordNum = document.createElement("div");
+      coordNum.classList.add("coord-num-cpu");
+      coordNum.textContent = counterCPU;
+      cell.appendChild(coordNum);
+    }
+
     cell.setAttribute("id", `computer ${elem}`);
     playerTwoBoard.appendChild(cell);
   });
